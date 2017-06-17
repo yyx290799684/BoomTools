1f2b6747a5b4c2ad5fe9bab9b16cbc7651c2548f
yyx <yyx290799684@hotmail.com>
2017-06-17 12:07:30 +0800

    添加项目文件。

---
+++ BoomTools/RegexHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BoomTools
+{
+    public class RegexHelper
+    {
+        /// <summary>
+        /// 是否为中文
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static bool IsChinese(string content)
+        {
+            //汉字
+            Regex reg = new Regex(@"^[\u4e00-\u9fa5]{0,}$");
+            return reg.IsMatch(content);
+        }
+    }
+}
